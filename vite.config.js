3a8cc2f1d3894b6971a0d63e13c34e76b8cfa9fd
samuelaudic <samuelaudic@gmail.com>
2024-04-17 09:33:02 +0200

    init commit ex 3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/hetic_react/",
-  plugins: [react()],
+  plugins: [react()]
 });
